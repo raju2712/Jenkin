0beeddc39218efae21963e1064c66ff28086fbf2
RajuR <163400512+raju2712@users.noreply.github.com>
2025-03-04 13:30:32 +0530

    Jenkin

---
+++ SampleProject/src/test/java/Contact/CreateContactTest.java
@@ -0,0 +1,16 @@
+package Contact;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+
+	@Test
+	public void createContactTest() {
+		System.out.println("Execute createContactTest");
+	}
+	
+	@Test
+	public void createContactWithDateTest() {
+		System.out.println("Execute createContactWithDateTest");
+	}
+}
---
+++ SampleProject/src/test/java/Organization/CreateOrgTest.java
@@ -0,0 +1,16 @@
+package Organization;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+
+	@Test
+	public void createOrgTest() {
+		System.out.println("Execute createOrgTest");
+	}
+	
+	@Test
+	public void createOrgWithIndTest() {
+		System.out.println("Execute createOrgWithIndTest");
+	}
+}
