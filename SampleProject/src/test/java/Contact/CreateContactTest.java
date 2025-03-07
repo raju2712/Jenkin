19327235f7933a09651a192f63f1cced21402614
raju2712 <rajuramikpm@gmail.com>
2025-03-07 10:35:01 +0530

    abc

--- SampleProject/src/test/java/Contact/CreateContactTest.java
+++ SampleProject/src/test/java/Contact/CreateContactTest.java
@@ -7,15 +7,27 @@ public class CreateContactTest {
 	@Test
 	public void createContactTest() {
 		System.out.println("Execute createContactTest");
+		String BROWSER = System.getProperty("Browser");
+		System.out.println(BROWSER);
+		String URL = System.getProperty("Url");
+		System.out.println(URL);
 	}
 	
 	@Test
 	public void createContactWithDateTest() {
+		String BROWSER = System.getProperty("Browser");
+		System.out.println(BROWSER);
+		String URL = System.getProperty("Url");
+		System.out.println(URL);
 		System.out.println("Execute createContactWithDateTest");
 	}
 	
 	@Test
 	public void deleteContact() {
+		String BROWSER = System.getProperty("Browser");
+		System.out.println(BROWSER);
+		String URL = System.getProperty("Url");
+		System.out.println(URL);
 		System.out.println("Execute deleteContact");
 	}
 }
--- SampleProject/src/test/java/Organization/CreateOrgTest.java
+++ SampleProject/src/test/java/Organization/CreateOrgTest.java
@@ -6,11 +6,19 @@ public class CreateOrgTest {
 
 	@Test
 	public void createOrgTest() {
+		String BROWSER = System.getProperty("Browser");
+		System.out.println(BROWSER);
+		String URL = System.getProperty("Url");
+		System.out.println(URL);
 		System.out.println("Execute createOrgTest");
 	}
 	
 	@Test
 	public void createOrgWithIndTest() {
+		String BROWSER = System.getProperty("Browser");
+		System.out.println(BROWSER);
+		String URL = System.getProperty("Url");
+		System.out.println(URL);
 		System.out.println("Execute createOrgWithIndTest");
 	}
 }
